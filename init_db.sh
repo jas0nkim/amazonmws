b537b4e0cf1958534cbfc9935e33e158a1b71198
Jason Kim <jason.kim.jiho@gmail.com>
2015-09-29 16:30:43 -0400

    bug fixed

--- init_db.sh
+++ init_db.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-mysql -u atewriteuser -p -e "CREATE DATABASE `amazonmws` CHARACTER SET utf8 COLLATE utf8_general_ci;"
+stty -echo
+printf "database password? "
+read INPUT_PASS
+stty echo
+printf "\n"
 
-SQLFILES=./db_updates/*.sql | sort -n
+mysql -u atewriteuser -p$INPUT_PASS -e "CREATE DATABASE amazonmws CHARACTER SET utf8 COLLATE utf8_general_ci;"
 
-for f in SQLFILES
+SQLFILES=./db_updates/*.sql
+
+for f in $SQLFILES
 do
 	echo "Proceessing $f file..."
 	cat $f
 
-	mysql -u atewriteuser -p amazonmws < $f
+	mysql -u atewriteuser -p$INPUT_PASS amazonmws < $f
 done
