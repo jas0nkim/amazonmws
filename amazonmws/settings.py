c04a5e5521cd43de37fa72c0d29aea041a80e7a9
Jason Kim <jason.kim.jiho@gmail.com>
2015-10-02 18:30:14 -0400

    fixed ebay base link for sandbox mode

--- amazonmws/settings.py
+++ amazonmws/settings.py
@@ -40,7 +40,7 @@ AMAZON_MARKETPLACE_ID = "ATVPDKIKX0DER" # US
 
 
 EBAY_ITEM_DEFAULT_QUANTITY = 20
-EBAY_ITEM_LINK_PREFIX = 'http://cgi.ebay.com/ws/eBayISAPI.dll?ViewItem&item='
+EBAY_ITEM_LINK_PREFIX = 'http://cgi.sandbox.ebay.com/ws/eBayISAPI.dll?ViewItem&item=' if APP_ENV == "stage" else 'http://cgi.ebay.com/ws/eBayISAPI.dll?ViewItem&item='
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'amazonmws (+http://www.yourdomain.com)'
