f32a1b8cbc5b3d886fd2f04f33ae1b783d48e464
Jason Kim <jason.kim.jiho@gmail.com>
2015-10-22 20:06:30 -0400

    namespacing bug fixed

--- notification_listener/__bootstrap.php
+++ notification_listener/__bootstrap.php
@@ -15,30 +15,29 @@ defined('EBNL_AMWS_PATH') or define('EBNL_AMWS_PATH', realpath($master . '/../am
 require_once(EBNL_VENDOR_PATH . '/autoload.php');
 
 // app config
-$app_config = Symfony\Component\Yaml\Yaml::parse(file_get_contents(EBNL_CONFIG_PATH . '/app.yaml'));
+$app_config = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(EBNL_CONFIG_PATH . '/app.yaml'));
 defined('APP_ENV') or define('APP_ENV', $app_config['env']);
 defined('APP_LOG_SERVER_HOST') or define('APP_LOG_SERVER_HOST', $app_config['log_server']['host']);
 defined('APP_LOG_SERVER_PORT') or define('APP_LOG_SERVER_PORT', $app_config['log_server']['port']);
 defined('APP_LOG_LEVEL') or define('APP_LOG_LEVEL', APP_ENV == "stage" ? \Monolog\Logger::DEBUG : \Monolog\Logger::ERROR);
-
 defined('APP_EBAY_NOTIFICATION_ENDPOINT_URL') or define('APP_EBAY_NOTIFICATION_ENDPOINT_URL', "/ebay/notification/listener");
 
 // log php errors
 function log_errors($severity, $message, $file, $line) {
 	if (!(error_reporting() & $severity)) {
         // This error code is not included in error_reporting
-        (new Amws\Logger())->error($message);
+        (new \Amws\Core\Logger())->error($message);
     }
-    (new Amws\Logger())->exception(new \ErrorException($message, 0, $severity, $file, $line));
+    (new \Amws\Core\Logger())->exception(new \ErrorException($message, 0, $severity, $file, $line));
 }
 set_error_handler('log_errors');
 
 // routing starts
 use \NoahBuscher\Macaw\Macaw;
 
-Macaw::post(APP_EBAY_NOTIFICATION_ENDPOINT_URL, function() {
+\NoahBuscher\Macaw\Macaw::post(APP_EBAY_NOTIFICATION_ENDPOINT_URL, function() {
 	// ebay config
-	$ebay_config = Symfony\Component\Yaml\Yaml::parse(file_get_contents(EBNL_CONFIG_PATH . '/ebay.yaml'));
+	$ebay_config = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(EBNL_CONFIG_PATH . '/ebay.yaml'));
 	$site = APP_ENV == "stage" ? "api.sandbox.ebay.com" : "api.ebay.com";
 	$devid = $ebay_config[$site]['devid'];
 	$appid = $ebay_config[$site]['appid'];
@@ -57,5 +56,5 @@ Macaw::post(APP_EBAY_NOTIFICATION_ENDPOINT_URL, function() {
 	$server->handle();
 });
 
-Macaw::dispatch();
+\NoahBuscher\Macaw\Macaw::dispatch();
 
