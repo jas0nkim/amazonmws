0c18767b6a74f704585ba74b2f405458e451b835
Jason Kim <jason.kim.jiho@gmail.com>
2015-10-26 07:38:53 -0400

    bug fixed

--- amazonmws/settings.py
+++ amazonmws/settings.py
@@ -494,8 +494,7 @@ EBAY_STORE_DEFAULT_POLICY_PAYMENT = """<p>We only accept Paypal. Credit Card Pay
 
 EBAY_STORE_DEFAULT_POLICY_RETURN = """<p>We fully guarantee all of our items. All items are Brand new and unused. 14 days refunds - we accept returns with defective or being pre-authorized. 10 percent restocking fee may apply.  Please contact us to get an authorization and returning address before sending the item back. Please leave a note with your eBay ID along with the returned item. Buyers pay shipping fees at their own cost to return products for exchange or refund. We will be responsible for the postage of replacements sending out.</p>"""
 
-EBAY_STORE_DEFAULT_ITEM_DESCRIPTION_TEMPLATE = """%s
-<div class="container-fluid">
+__default_description_template = """<div class="container-fluid">
     {% if description and description != ""  %}
     <div class="panel panel-rfi">
         <div class="panel-heading">Description</div>
@@ -537,4 +536,8 @@ EBAY_STORE_DEFAULT_ITEM_DESCRIPTION_TEMPLATE = """%s
     </div>
     {% endif %}
 </div>
-%s""" % (EBAY_ITEM_DESCRIPTION_CSS, EBAY_ITEM_DESCRIPTION_JS)
+"""
+
+EBAY_STORE_DEFAULT_ITEM_DESCRIPTION_TEMPLATE = """%s
+%s
+%s""" % (EBAY_ITEM_DESCRIPTION_CSS, __default_description_template, EBAY_ITEM_DESCRIPTION_JS)
