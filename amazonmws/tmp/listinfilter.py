f3507c6ffe21d4b9b1c40530ba653ba07f015e0d
Jason Kim <jason.kim.jiho@gmail.com>
2015-10-28 10:12:04 -0400

    tmp script for ebaylisting filter

---
+++ amazonmws/tmp/listinfilter.py
@@ -0,0 +1,22 @@
+import sys, os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+import csv
+
+from storm.exceptions import StormError
+
+from amazonmws import utils
+from amazonmws.models import StormStore, AmazonItem, AmazonItemPicture, Scraper, EbayItem, Task, ItemQuantityHistory, EbayStore, LookupAmazonItem, Lookup, LookupOwnership
+
+with open('EBAYListing.csv', 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        try:
+            item = StormStore.find(EbayItem, EbayItem.ebay_store_id == 3, EbayItem.ebid == utils.str_to_unicode(row[0]).strip()).one()
+            if item:
+                print item.ebid
+            else:
+                continue
+        except StormError:
+            continue
