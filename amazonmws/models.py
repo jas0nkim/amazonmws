458279da7f53f6e47a543a12079ca6c2d8abf5e0
Jason Kim <jason.kim.jiho@gmail.com>
2015-10-29 11:15:34 -0400

    bug fixed

--- amazonmws/models.py
+++ amazonmws/models.py
@@ -91,6 +91,8 @@ class Task(object):
     ebay_task_monitoring_quantity_changes = 4
     ebay_task_monitoring_amazon_items = 5
     ebay_task_revise_item = 6
+    
+    amazon_task_exclude_item = 1001
 
     task_names = {
         1: "ebay_task__listing",
@@ -98,7 +100,8 @@ class Task(object):
         3: "ebay_task__monitoring_amazon_item_status_changes",
         4: "ebay_task__monitoring_ebay_item_quantity",
         5: "ebay_task__monitoring_amazon_items",
-        6: "ebay_task__revise_item",
+
+        1001: "amazon_task__exclude_item",
     }
 
     @staticmethod
--- amazonmws/monitor/exclude_amazon_items.py
+++ amazonmws/monitor/exclude_amazon_items.py
@@ -2,8 +2,15 @@ import sys, os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
+from storm.exceptions import StormError
+from amazonmws.models import StormStore, AmazonItem, LookupAmazonItem, EbayItem, Task
+from amazonmws.loggers import GrayLogger as logger, StaticFieldFilter, get_logger_name
+
 
 def exclude_amazin_item(amazon_item):
+    
+    logger.addFilter(StaticFieldFilter(get_logger_name(), Task.get_name(Task.amazon_task_exclude_item)))
+
     try:
         amazon_item.status = AmazonItem.STATUS_EXCLUDED
         StormStore.add(amazon_item)
@@ -14,20 +21,21 @@ def exclude_amazin_item(amazon_item):
         return False
     
     ebay_item = get_ebay_item(amazon_item)
-    try:
-        ebay_item.status = EbayItem.STATUS_INACTIVE
-        StormStore.add(ebay_item)
-        StormStore.commit()
-    except StormError, e:
-        logger.exception("[EBID: " + ebay_item.ebid + "] " + "Failed to update status to STATUS_INACTIVE")
-        StormStore.rollback()
-        return False
+    if ebay_item != None:
+        try:
+            ebay_item.status = EbayItem.STATUS_INACTIVE
+            StormStore.add(ebay_item)
+            StormStore.commit()
+        except StormError, e:
+            logger.exception("[EBID: " + ebay_item.ebid + "] " + "Failed to update status to STATUS_INACTIVE")
+            StormStore.rollback()
+            return False
     return True
 
 def get_ebay_item(amazon_item):
     ebay_item = None
     try:
-        ebay_item = StormStore.find(EbayItem, EbayItem.amazon_item_id = amazon_item.id).one()
+        ebay_item = StormStore.find(EbayItem, EbayItem.amazon_item_id == amazon_item.id).one()
     except StormError, e:
         logger.exception("[ASIN: " + amazon_item.asin + "] " + "Failed to fetch related ebay item")
     return ebay_item
