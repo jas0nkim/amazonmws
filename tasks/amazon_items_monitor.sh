b842ac76e98edd182fad75a6ff914d3c99dc4c72
Jason Kim <jason.kim.jiho@gmail.com>
2015-10-30 10:57:46 -0400

    set PATH to all task scripts

--- tasks/amazon_items_monitor.sh
+++ tasks/amazon_items_monitor.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PATH=/virtualenvs/amazonmws/bin:/home/jason/.local/bin:/home/jason/.local/bin:/home/jason/.local/bin:/home/jason/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+
 source /home/jason/.profile
 cd /applications/amazonmws
 /virtualenvs/amazonmws/bin/python /applications/amazonmws/amazonmws/monitor/amazon_item_monitor.py
