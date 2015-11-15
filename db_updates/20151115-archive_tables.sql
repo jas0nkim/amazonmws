ebff8dc929afed373ec2a3df5aa6dd44f289902b
Jason Kim <jason.kim.jiho@gmail.com>
2015-11-15 06:32:05 -0500

    archive tables

---
+++ db_updates/20151115-archive_tables.sql
@@ -0,0 +1,4 @@
+RENAME TABLE `amazon_item_pictures` to `__amazon_item_pictures`;
+RENAME TABLE `amazon_items` to `__amazon_items`;
+RENAME TABLE `lookup_amazon_items` to `__lookup_amazon_items`;
+RENAME TABLE `lookup_ownerships` to `__lookup_ownerships`;
