cf3540ac4d10951071cdf1d7bedc05c64732b591
Jason Kim <jason.kim.jiho@gmail.com>
2015-11-18 23:04:50 -0500

    added lockfile for the crontab script

--- scrapers/amzn/amzn/bin/run_bestseller.py
+++ scrapers/amzn/amzn/bin/run_bestseller.py
@@ -5,22 +5,28 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scrapy.utils.log import configure_logging
 
+from amazonmws import utils as amazonmws_utils
 from amazonmws.loggers import set_root_graylogger
 
 
 if __name__ == "__main__":
+    lock_filename = 'bestseller_scrapy.lock'
+    amazonmws_utils.check_lock(lock_filename)
     # configure_logging(install_root_handler=False)
     # set_root_graylogger()
 
-    process = CrawlerProcess(get_project_settings())
-    # process.crawl('amazon_bestseller', 
-    #     start_urls=['http://www.amazon.com/Best-Sellers/zgbs',])
-    process.crawl('amazon_bestseller', 
-        start_urls=['http://www.amazon.com/Best-Sellers-Baby/zgbs/baby-products',
-        	'http://www.amazon.com/Best-Sellers-Automotive/zgbs/automotive',
-        	'http://www.amazon.com/Best-Sellers-Beauty-Tools-Accessories/zgbs/beauty/11062741',
-        	'http://www.amazon.com/Best-Sellers-Arts-Crafts-Sewing/zgbs/arts-crafts',
-        	'http://www.amazon.com/Best-Sellers-Electronics-Computers-Accessories/zgbs/electronics/541966',
-        	'http://www.amazon.com/Best-Sellers-Toys-Games/zgbs/toys-and-games',
-        	'http://www.amazon.com/Best-Sellers-Kitchen-Dining-Utensils-Gadgets/zgbs/kitchen/289754',])
-    process.start()
+    try:
+        process = CrawlerProcess(get_project_settings())
+        # process.crawl('amazon_bestseller', 
+        #     start_urls=['http://www.amazon.com/Best-Sellers/zgbs',])
+        process.crawl('amazon_bestseller', 
+            start_urls=['http://www.amazon.com/Best-Sellers-Baby/zgbs/baby-products',
+                'http://www.amazon.com/Best-Sellers-Automotive/zgbs/automotive',
+                'http://www.amazon.com/Best-Sellers-Beauty-Tools-Accessories/zgbs/beauty/11062741',
+                'http://www.amazon.com/Best-Sellers-Arts-Crafts-Sewing/zgbs/arts-crafts',
+                'http://www.amazon.com/Best-Sellers-Electronics-Computers-Accessories/zgbs/electronics/541966',
+                'http://www.amazon.com/Best-Sellers-Toys-Games/zgbs/toys-and-games',
+                'http://www.amazon.com/Best-Sellers-Kitchen-Dining-Utensils-Gadgets/zgbs/kitchen/289754',])
+        process.start()
+    finally:
+        amazonmws_utils.release_lock(lock_filename)
