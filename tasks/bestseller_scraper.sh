c267b85e2b99f2479a7fa0ede086dc02f4461369
Jason Kim <jason.kim.jiho@gmail.com>
2015-11-18 22:55:46 -0500

    added bestseller crontab

---
+++ tasks/bestseller_scraper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PATH=/virtualenvs/amazonmws/bin:/home/jason/.local/bin:/home/jason/.local/bin:/home/jason/.local/bin:/home/jason/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+
+source /home/jason/.profile
+cd /applications/amazonmws/scrapers/amzn/amzn/bin
+/virtualenvs/amazonmws/bin/python /applications/amazonmws/scrapers/amzn/amzn/bin/run_bestseller.py
