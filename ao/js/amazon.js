1fa246dc8bf8979c23f3aa137aa09e867bc974aa
Jason Kim <jason.kim.jiho@gmail.com>
2015-11-19 21:01:00 -0500

    working on auto-ordering

--- ao/js/amazon.js
+++ ao/js/amazon.js
@@ -1,4 +1,4 @@
-var casper = require('casper').create({
+var options = {
     verbose: true,
     logLevel: "debug",
     pageSettings: {
@@ -7,10 +7,26 @@ var casper = require('casper').create({
     },
     
     __takeScreenshots: true,
-    __screenshotsFolder: '../../ss/',
+    __screenshotsFolder: '../../ss/',    
+}
 
-});
+var casper = require('casper').create(options);
 
+var input = {
+    // amazon auth
+    amazon_user: casper.cli.get("amazon_user"),
+    amazon_pass: casper.cli.get("amazon_pass"),
+    
+    // buyer shipping info
+    buyer_name: casper.cli.get("buyer_name"),
+    buyer_shipping_address_1: casper.cli.get("buyer_shipping_address_1"),
+    buyer_shipping_address_2: casper.cli.get("buyer_shipping_address_2"),
+    buyer_shipping_city: casper.cli.get("buyer_shipping_city"),
+    buyer_shipping_state: casper.cli.get("buyer_shipping_state"),
+    buyer_shipping_zip: casper.cli.get("buyer_shipping_zip"),
+    buyer_shipping_country: casper.cli.get("buyer_shipping_country"),
+    buyer_shipping_phone: casper.cli.get("buyer_shipping_phone"),
+};
 
 // function getLinks() {
 //     var links = document.querySelectorAll('h3.r a');
@@ -67,30 +83,97 @@ casper.start('http://www.amazon.com/dp/B00KKV86MI', function() {
     // screen 3: Sign In
     // 
     this.then(function() {
-
         this.waitForSelector('form[name="signIn"]', function() {
-            this.fillSelectors('form[name="signIn"]', {
-                'input[name="email"]': 'xxxx',
-                'input[name="password"]': 'xxxx',
-            });
+            this.fill('form[name="signIn"]', {
+                'email': input.amazon_user,
+                'password': input.amazon_pass,
+            }, true);
 
             this.echo('Logging In...');
-            this.click('form[name="signIn"] input#signInSubmit');
         });
     });
 
     //
     // screen 4: Checkout
     // 
+
+    // 4.1. Choose a shipping address
     this.then(function() {
-        this.waitForSelector('#bottomsubtotals', function() {
+        this.waitForSelector('#add-address-popover-link', function() {
             // screenshot taken
-            ss();
+            // ss();
+
+            this.thenClick('#add-address-popover-link', function() {
+                this.waitForSelector('form#domestic-address-popover-form', function() {
+                    this.fill('form#domestic-address-popover-form', {
+                        'enterAddressFullName': input.buyer_name,
+                        'enterAddressAddressLine1': input.buyer_shipping_address_1,
+                        'enterAddressAddressLine2': input.buyer_shipping_address_2,
+                        'enterAddressCity': input.buyer_shipping_city,
+                        'enterAddressStateOrRegion': input.buyer_shipping_state,
+                        'enterAddressPostalCode': input.buyer_shipping_zip,
+                        // 'input[name="enterAddressCountryCode"]': input.buyer_shipping_country,
+                        'enterAddressPhoneNumber': input.buyer_shipping_phone,
+                    }, false);
+
+                    this.echo('Filling shipping address information...');
+                    this.click('.a-popover-footer input[type="submit"]');
+                });
+            });
         }, function() {
-            // max timeout: default is 5 sec. increased to 20 sec here
-            return 20000;
+            // max timeout: default is 5 sec. increased to 10 sec here
+            return 10000;
+        });
+
+    });
+
+    // 4.2. Choose a payment method
+    this.then(function() {
+        this.waitForSelector('#existing-payment-methods', function() {
+
+            this.click('#existing-balance input#pm_gc_radio');
+
+            this.echo('Filling shipping address information...');
+            this.click('#useThisPaymentMethodButtonId input#continue-top');
         });
     });
+
+    // 4.3. Review items and shipping
+    this.then(function() {
+        this.waitForSelector('#spc-orders', function() {
+
+            this.thenBypassUnless(function() {
+                return this.exists('#spc-orders span.gift-popover-link a')
+            }, 1)
+
+            // gift card option
+            this.thenClick('#spc-orders span.gift-popover-link a', function() {
+                this.waitForSelector('.popover-gift.checkout', function() {
+                    this.evaluate(function(term) {
+                        document.querySelector('textarea[name="gift-message-text"]').setAttribute('value', term);
+                    }, ''); // set blank
+
+                    this.then(function() {
+                        this.click('.set-gift-options-button input[type="submit"]');
+                    });
+                });
+            });
+        });
+    });
+
+    // 4.4. Place your order
+    this.then(function() {
+        this.click('input[name="placeYourOrder1"]');
+    });
+
+    // 5. Thank you, your order has been placed.
+    this.then(function() {
+        this.waitForText('your order has been placed', function() {
+            var order_number = this.getElementInfo('h5 > span.a-text-bold').text;
+            this.echo(order_number)
+        }, 10000);
+    },);
+
 
 }).on("url.changed", function() {
     
