eaeee3be0295e757e74b7cb6f6e7b4050be69af4
Jason Kim <jason.kim.jiho@gmail.com>
2015-11-26 06:22:26 -0500

    added more preferred_categories for wbjworld

--- scrapers/amzn/amzn/bin/run_categoryitems.py
+++ scrapers/amzn/amzn/bin/run_categoryitems.py
@@ -16,8 +16,17 @@ if __name__ == "__main__":
     process = CrawlerProcess(get_project_settings())
     process.crawl('amazon_base', 
         start_urls=[
-        	'http://www.amazon.com/b/?ie=UTF8&node=12896641',
-        	'http://www.amazon.com/cookware/b/?ie=UTF8&node=289814',
+            # 'http://www.amazon.com/b/?ie=UTF8&node=12896641',
+            # 'http://www.amazon.com/cookware/b/?ie=UTF8&node=289814',
+            'http://www.amazon.com/b/?node=2975505011&ie=UTF8',
+            'http://www.amazon.com/b/?node=2975434011&ie=UTF8',
+            'http://www.amazon.com/b/?node=7955292011&ie=UTF8',
+            'http://www.amazon.com/b/?node=2975462011&ie=UTF8',
+            'http://www.amazon.com/b/?node=2975478011&ie=UTF8',
+            'http://www.amazon.com/b/?node=2975309011&ie=UTF8',
+            'http://www.amazon.com/b/?node=2975242011&ie=UTF8',
+            'http://www.amazon.com/b/?node=2975268011&ie=UTF8',
+            'http://www.amazon.com/b/?node=2975250011&ie=UTF8',
         ])
     process.start()
 
---
+++ tools/db_migration/20151126-insert-preferred_categories.sql
@@ -0,0 +1,583 @@
+-- amazon_bestseller
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Furniture',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Bath Products',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Kids\' Bath',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Kids\' Bath Towels',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Kids\' Shower Accessories',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Watches',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Appliances',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Toys & Games',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Toy Figures & Playsets',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Action & Toy Figures',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Action Figure Vehicles',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Computers & Accessories',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'External Components',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'External Sound Cards',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'External TV Tuners',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Graphics Card Fans',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Water Cooling Systems',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Internal Hard Drive Cooling Fans',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon_bestseller',
+	'Portable Audio & Video',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+-- amazon
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Reptiles & Amphibians : Food',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Dogs : Treats',
+	'80',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Fish & Aquatic Pets : Aquariums & Fish Bowls',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Fish & Aquatic Pets : Aquariums & Fish Bowls',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Fish & Aquatic Pets : Automatic Feeders',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Fish & Aquatic Pets : Aquarium Water Treatments',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Cats : Treats',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Cats : Apparel',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Dogs : Grooming',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Cats : Carriers & Strollers',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
