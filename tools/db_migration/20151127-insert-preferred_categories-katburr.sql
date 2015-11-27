2e98d54f2f6719ba2772a01f278b58192a8e2527
Jason Kim <jason.kim.jiho@gmail.com>
2015-11-27 17:26:04 -0500

    adding pref. categories for kat.burr

---
+++ tools/db_migration/20151127-insert-preferred_categories-katburr.sql
@@ -0,0 +1,79 @@
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'2',
+	'amazon_bestseller',
+	'Computers & Accessories',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'2',
+	'amazon_bestseller',
+	'Data Storage',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'2',
+	'amazon_bestseller',
+	'Laptop Accessories',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'2',
+	'amazon_bestseller',
+	'Dolls & Accessories',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
