978deba9ef04c53615a72ee7c0fc24193428af1b
Jason Kim <jason.kim.jiho@gmail.com>
2015-11-27 04:28:30 -0500

    updated run_categoryitems - for dae0c

--- scrapers/amzn/amzn/bin/run_categoryitems.py
+++ scrapers/amzn/amzn/bin/run_categoryitems.py
@@ -18,15 +18,22 @@ if __name__ == "__main__":
         start_urls=[
             # 'http://www.amazon.com/b/?ie=UTF8&node=12896641',
             # 'http://www.amazon.com/cookware/b/?ie=UTF8&node=289814',
-            'http://www.amazon.com/b/?node=2975505011&ie=UTF8',
-            'http://www.amazon.com/b/?node=2975434011&ie=UTF8',
-            'http://www.amazon.com/b/?node=7955292011&ie=UTF8',
-            'http://www.amazon.com/b/?node=2975462011&ie=UTF8',
-            'http://www.amazon.com/b/?node=2975478011&ie=UTF8',
-            'http://www.amazon.com/b/?node=2975309011&ie=UTF8',
-            'http://www.amazon.com/b/?node=2975242011&ie=UTF8',
-            'http://www.amazon.com/b/?node=2975268011&ie=UTF8',
-            'http://www.amazon.com/b/?node=2975250011&ie=UTF8',
+
+            # 'http://www.amazon.com/b/?node=2975505011&ie=UTF8',
+            # 'http://www.amazon.com/b/?node=2975434011&ie=UTF8',
+            # 'http://www.amazon.com/b/?node=7955292011&ie=UTF8',
+            # 'http://www.amazon.com/b/?node=2975462011&ie=UTF8',
+            # 'http://www.amazon.com/b/?node=2975478011&ie=UTF8',
+            # 'http://www.amazon.com/b/?node=2975309011&ie=UTF8',
+            # 'http://www.amazon.com/b/?node=2975242011&ie=UTF8',
+            # 'http://www.amazon.com/b/?node=2975268011&ie=UTF8',
+            # 'http://www.amazon.com/b/?node=2975250011&ie=UTF8',
+
+            'https://www.amazon.com/s/?fst=as%3Aoff&rh=n%3A5174%2Cn%3A%2144259011%2Cn%3A%21251269011%2Cn%3A%21468038%2Cn%3A291920%2Cp_n_format_browse-bin%3A1294043011&bbn=291920&ie=UTF8&qid=1448422583&rnid=492502011',
+            'https://www.amazon.com/s/?fst=as%3Aoff&rh=n%3A283155%2Cn%3A%212334088011%2Cn%3A%212334119011%2Cn%3A13108091011%2Cn%3A13123727011%2Cp_n_condition-type%3A1294422011&bbn=13123727011&ie=UTF8&qid=1448422785&rnid=1294421011',
+            'http://www.amazon.com/s/?fst=as%3Aoff&rh=n%3A2625373011%2Cn%3A%212644981011%2Cn%3A%212644982011%2Cn%3A%212998369011%2Cn%3A501230%2Cp_n_format_browse-bin%3A2650305011%2Cp_n_price_fma%3A10346819011&bbn=501230&ie=UTF8&qid=1448423218&rnid=10346811011',
+            'http://www.amazon.com/s/?fst=as%3Aoff&rh=n%3A165793011%2Cn%3A%212334111011%2Cn%3A%212334142011%2Cn%3A9765225011%2Cp_6%3AATVPDKIKX0DER&bbn=9765225011&ie=UTF8&qid=1448423064&rnid=275224011',
+            'http://www.amazon.com/s/?fst=as%3Aoff&rh=n%3A16310101%2Cn%3A%212334096011%2Cn%3A%212334127011%2Cn%3A13130343011%2Cn%3A13130347011%2Cp_6%3AATVPDKIKX0DER&bbn=13130347011&ie=UTF8&qid=1448423143&rnid=698480011',
         ])
     process.start()
 
