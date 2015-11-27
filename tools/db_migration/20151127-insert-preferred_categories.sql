7162bfd68e56134c15c18c82a643eff2d87b4034
Jason Kim <jason.kim.jiho@gmail.com>
2015-11-27 05:02:47 -0500

    added ebay store entry - dae0c

---
+++ tools/db_migration/20151127-insert-preferred_categories.sql
@@ -0,0 +1,100 @@
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'4',
+	'amazon',
+	'CDs & Vinyl',
+	'50',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'4',
+	'amazon',
+	'Books',
+	'50',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'4',
+	'amazon',
+	'Movies & TV : Boxed Sets',
+	'50',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'4',
+	'amazon',
+	'Toys & Games',
+	'50',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'4',
+	'amazon',
+	'Grocery & Gourmet Food',
+	'50',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
