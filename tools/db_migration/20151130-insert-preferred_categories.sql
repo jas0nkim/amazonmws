cda54515418fd10ecbffa7c084adcf66336a1485
Jason Kim <jason.kim.jiho@gmail.com>
2015-11-30 06:08:39 -0500

    added preferred categories for wbjworld

---
+++ tools/db_migration/20151130-insert-preferred_categories.sql
@@ -0,0 +1,239 @@
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Grocery & Gourmet Food',
+	'300',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Grocery & Gourmet Food : Breakfast Foods',
+	'100',
+	'50',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Grocery & Gourmet Food : Cooking & Baking',
+	'100',
+	'50',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Grocery & Gourmet Food : Coffee, Tea & Beverages',
+	'100',
+	'50',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Grocery & Gourmet Food : Cooking & Baking : Cooking Oils, Vinegars & Sprays',
+	'100',
+	'50',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Cats : Grooming',
+	'200',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Cats : Grooming : Claw Care',
+	'100',
+	'50',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Small Animals',
+	'300',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Small Animals : Treats',
+	'100',
+	'50',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Small Animals : Feeding & Watering Supplies',
+	'100',
+	'50',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Small Animals : Grooming',
+	'100',
+	'50',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Pet Supplies : Small Animals : Odor & Stain Removers',
+	'100',
+	'50',
+	'1',
+	NOW(),
+	NOW()
+);
