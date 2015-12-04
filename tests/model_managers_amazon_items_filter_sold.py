0fdaad6b9e09f3d3378831d4491136ed3b84c9ef
Jason Kim <jason.kim.jiho@gmail.com>
2015-12-04 17:11:23 +0000

    fix/update model_managers_amazon_items_filter_sold.py

--- tests/model_managers_amazon_items_filter_sold.py
+++ tests/model_managers_amazon_items_filter_sold.py
@@ -14,11 +14,14 @@ from amazonmws.loggers import GrayLogger as logger
 def fetch_sold_for_listing():
     """fetch amazon items which sold by sellers in system - order by num of sold
     """
-    query = 'select am.*, count(*) as count from transactions t left join ebay_items eb on t.item_id = eb.ebid left join zz__amazon_items am on eb.asin = am.asin where am.asin is not null group by eb.asin order by count;'
+    query = 'select am.asin, count(*) as count from transactions t left join ebay_items eb on t.item_id = eb.ebid left join zz__amazon_items am on eb.asin = am.asin where am.asin is not null group by eb.asin order by count'
 
-    results = StormStore.execute(Select(SQLRaw(query)))
+    results = StormStore.execute(SQLRaw("(%s)" % query)).get_all()
+    for result in results:
+        """result: tuple
+        """
+    	print "asin: " + str(result[0]) + " --- count: " + str(result[1])
+    print "total: " + str(len(results))
 
-    print results
 
-    for result as results:
-    	print "asin: " + str(result.asin) + " --- count: " + str(result.count)
+fetch_sold_for_listing()
