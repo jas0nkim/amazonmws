4eebe00e83fc1a0ae46e812070083741d9abdc96
Jason Kim <jason.kim.jiho@gmail.com>
2015-12-04 10:36:51 -0500

    new cronjob added - run_monitor.py

---
+++ scrapers/amzn/amzn/bin/run_monitor.py
@@ -0,0 +1,30 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..'))
+
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from scrapy.utils.log import configure_logging
+
+from amazonmws import utils as amazonmws_utils
+from amazonmws.loggers import set_root_graylogger, GrayLogger as logger
+from amazonmws.model_managers import *
+
+
+if __name__ == "__main__":
+    lock_filename = 'monitor.lock'
+    amazonmws_utils.check_lock(lock_filename)
+
+    try:    
+        # configure_logging(install_root_handler=False)
+        # set_root_graylogger()
+
+        items = EbayItemModelManager.fetch_distinct_asin()
+        if items.count() > 0:
+            process = CrawlerProcess(get_project_settings())
+            process.crawl('amazon_asin', asins=[x.asin for x in items])
+            process.start()
+        else:
+            logger.error('No amazon items found')
+    finally:
+        amazonmws_utils.release_lock(lock_filename)
+
