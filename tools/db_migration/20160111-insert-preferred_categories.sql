d208b6520b9fb0a2ece6e71ef4a60a19a51dcebf
Jason Kim <jason.kim.jiho@gmail.com>
2016-01-11 15:36:35 -0500

    updated pref. category

---
+++ tools/db_migration/20160111-insert-preferred_categories.sql
@@ -0,0 +1,739 @@
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Arts, Crafts & Sewing : Beading & Jewelry Making : Jewelry Making Kits',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Arts, Crafts & Sewing : Beading & Jewelry Making : Metal Stamping Tools',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Arts, Crafts & Sewing : Beading & Jewelry Making : Polishing & Buffing',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Arts, Crafts & Sewing : Beading & Jewelry Making : Purse Making',
+	'100',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Beauty : Hair Care : Tools & Appliances : Curling Irons & Wands',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Beauty : Hair Care : Tools & Appliances : Diffusers & Dryer Attachments',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Beauty : Hair Care : Tools & Appliances : Hair Brushes',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Beauty : Hair Care : Tools & Appliances : Hair Combs',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Beauty : Hair Care : Tools & Appliances : Hair Cutting Tools : Hair Cutting Kits',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Beauty : Hair Care : Tools & Appliances : Hair Cutting Tools : Shears',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Beauty : Hair Care : Tools & Appliances : Hair Dryers',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Beauty : Hair Care : Tools & Appliances : Hair Rollers',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Beauty : Hair Care : Tools & Appliances : Straighteners',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Beauty : Tools & Accessories : Trimmers & Clippers : Hair Clippers & Accessories',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Electronics : Accessories & Supplies : Audio & Video Accessories : Cables & Interconnects : Power Cables',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Electronics : Computers & Accessories : Computer Accessories & Peripherals : Cables & Interconnects',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Electronics : Computers & Accessories : Computer Accessories & Peripherals : Cables & Interconnects : Ethernet Cables',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Electronics : Computers & Accessories : Computer Accessories & Peripherals : Cables & Interconnects : Ethernet Cables : Cat 5 Cables',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Electronics : Computers & Accessories : Computer Accessories & Peripherals : Cables & Interconnects : Ethernet Cables : Cat 5e Cables',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Electronics : Computers & Accessories : Computer Accessories & Peripherals : Cables & Interconnects : Ethernet Cables : Cat 6 Cables',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Electronics : Computers & Accessories : Computer Accessories & Peripherals : Cables & Interconnects : Lightning Cables',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Electronics : Computers & Accessories : Computer Accessories & Peripherals : Cables & Interconnects : Mini-SAS Cables',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Electronics : Computers & Accessories : Computer Accessories & Peripherals : Cables & Interconnects : Modem Cables',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Electronics : Computers & Accessories : Computer Accessories & Peripherals : Cables & Interconnects : Parallel Cables',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Electronics : Computers & Accessories : Computer Accessories & Peripherals : Cables & Interconnects : SATA Cables',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Electronics : Computers & Accessories : Computer Accessories & Peripherals : Cables & Interconnects : Serial Cables',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Electronics : Computers & Accessories : Computer Accessories & Peripherals : Cables & Interconnects : USB Cables',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Electronics : Computers & Accessories : Computer Accessories & Peripherals : Cables & Interconnects : Thunderbolt Cables',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Health & Personal Care : Household Supplies : Paper & Plastic : Trash Bags',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Health & Personal Care : Household Supplies : Paper & Plastic : Trash Bags : Large Kitchen Bags',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Health & Personal Care : Household Supplies : Paper & Plastic : Trash Bags : Lawn & Leaf Bags',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Health & Personal Care : Household Supplies : Paper & Plastic : Trash Bags : Tall Kitchen Bags',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Health & Personal Care : Household Supplies : Paper & Plastic : Trash Bags : Wastebasket Bags',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Home & Kitchen : Bath : Bath Rugs',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Home & Kitchen : Bath : Bathroom Accessories : Bathroom Accessory Sets',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Home & Kitchen : Bath : Bathroom Accessories : Makeup Organizers',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'3',
+	'amazon',
+	'Home & Kitchen : Bath : Bathroom Accessories : Shower Accessories : Shower Caddies',
+	'110',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
