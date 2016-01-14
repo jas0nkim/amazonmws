64a2c178c4b6e1c57bd14bb2aad8f6213cd62c59
Jason Kim <jason.kim.jiho@gmail.com>
2016-01-14 16:23:09 -0500

    pref. categories, bestseller, added

---
+++ tools/db_migration/20160114-insert-preferred_categories.sql
@@ -0,0 +1,340 @@
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Coffee, Tea & Espresso',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Coffee & Tea',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Coffee Beverages',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Tea Beverages',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Dry Dog Food',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Dog Treats',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Dry Cat Food',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Cat Treats',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Baby & Toddler Feeding Supplies',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Pregnancy & Maternity Products',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Baby Gear',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Baby Health & Care Products',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Baby Safety Products',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Cell Phone Accessories',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Cell Phone Batteries & Battery Packs',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Bluetooth Cell Phone Headsets',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `zz__ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Wired Cell Phone Headsets',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
