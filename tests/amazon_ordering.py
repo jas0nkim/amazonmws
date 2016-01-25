0f5d2ad9f498fbe5e5cc0c69f7958bc283a9af83
Jason Kim <jason.kim.jiho@gmail.com>
2016-01-25 19:21:25 +0000

    removed unused import lib

--- tests/amazon_ordering.py
+++ tests/amazon_ordering.py
@@ -2,8 +2,6 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'tasks'))
 
-from storm.exceptions import StormError
-
 from amazonmws import settings, utils
 from amazonmws.loggers import GrayLogger as logger, StaticFieldFilter, get_logger_name
 from amazonmws.model_managers import *
