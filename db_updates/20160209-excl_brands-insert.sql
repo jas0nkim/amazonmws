2e23d68652d55c0dcaabdbfc6185caaab384eb5c
Jason Kim <jason.kim.jiho@gmail.com>
2016-02-12 20:59:58 +0000

    doc updated. excluding brands added

--- db_updates/20160209-excl_brands-insert.sql
+++ db_updates/20160209-excl_brands-insert.sql
@@ -6,3 +6,15 @@ INSERT INTO `excl_brands` (`brand_name`, `category`, `created_at`, `updated_at`)
 
 -- Orowix
 INSERT INTO `excl_brands` (`brand_name`, `category`, `created_at`, `updated_at`) VALUES ('Orowix', 'Health & Personal Care', NOW(), NOW());
+
+-- Cellucor
+INSERT INTO `excl_brands` (`brand_name`, `category`, `created_at`, `updated_at`) VALUES ('Cellucor', 'Health & Personal Care', NOW(), NOW());
+
+-- OXO in Health & Personal Care
+INSERT INTO `excl_brands` (`brand_name`, `category`, `created_at`, `updated_at`) VALUES ('OXO', 'Health & Personal Care', NOW(), NOW());
+
+-- Victorio Kitchen Products
+INSERT INTO `excl_brands` (`brand_name`, `category`, `created_at`, `updated_at`) VALUES ('Victorio Kitchen Products', 'Home & Kitchen', NOW(), NOW());
+
+-- Linksys
+INSERT INTO `excl_brands` (`brand_name`, `category`, `created_at`, `updated_at`) VALUES ('Linksys', 'Electronics', NOW(), NOW());
