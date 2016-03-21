49b46843fcd03e620aa5401df46ec56b53b0bd52
Jason Kim <jason.kim.jiho@gmail.com>
2016-03-21 18:45:09 -0400

    added hotfix_ebay_item_force_end.py

---
+++ atoe/bin/hotfix_ebay_item_force_end.py
@@ -0,0 +1,28 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'scrapers', 'amzn'))
+
+from amazonmws import django_cli
+django_cli.execute()
+
+from amazonmws import settings as amazonmws_settings, utils as amazonmws_utils
+from amazonmws.loggers import GrayLogger as logger
+from amazonmws.model_managers import *
+
+from atoe.actions import EbayItemAction
+
+
+def run():
+    _ebay_item_id = '281946707127'
+
+    ebay_item = EbayItemModelManager.fetch_one(ebid=_ebay_item_id)
+
+    if not ebay_item:
+        return False
+
+    action = EbayItemAction(ebay_store=ebay_item.ebay_store, ebay_item=ebay_item)
+    action.end_item()
+
+
+if __name__ == "__main__":
+    run()
