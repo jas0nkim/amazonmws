10188cb47a59459dc9a55659212bdaa6a8da6186
Jason Kim <jason.kim.jiho@gmail.com>
2016-04-09 13:47:08 -0400

    applied premium service (crawlera) on scripts - run_monitor.py, run_repricer.py

--- scrapers/amzn/amzn/bin/run_monitor.py
+++ scrapers/amzn/amzn/bin/run_monitor.py
@@ -1,6 +1,8 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..'))
 
+import getopt
+
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scrapy.utils.log import configure_logging
@@ -13,15 +15,40 @@ from amazonmws.loggers import set_root_graylogger, GrayLogger as logger
 from amazonmws.model_managers import *
 
 
-if __name__ == "__main__":
+def main(argv):
+    is_premium = False
+    try:
+        opts, args = getopt.getopt(argv, "hs:", ["service=", ])
+    except getopt.GetoptError:
+        print 'run_monitor.py -s <basic|premium>'
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'run_monitor.py -s <basic|premium>'
+            sys.exit()
+        elif opt in ("-s", "--service") and arg == 'premium':
+            is_premium = True
+    run(premium=is_premium)
+
+def run(premium):
     # configure_logging(install_root_handler=False)
     # set_root_graylogger()
 
-    asins = EbayItemModelManager.fetch_distinct_asin()
+    asins = []
+
+    if premium:
+        asins = EbayItemModelManager.fetch_distinct_asin(ebay_store_id=1)
+    else:
+        asins = EbayItemModelManager.fetch_distinct_asin()
+
     if len(asins) > 0:
         process = CrawlerProcess(get_project_settings())
-        process.crawl('amazon_asin', asins=asins)
+        process.crawl('amazon_asin', asins=asins, premium=premium)
         process.start()
     else:
         logger.error('No amazon items found')
 
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
