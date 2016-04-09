2a605767162b751bbf355829561b3e6350785758
Jason Kim <jason.kim.jiho@gmail.com>
2016-04-09 17:49:23 -0400

    pref. categories added

---
+++ tools/db_migration/20160409-insert-preferred_categories.sql
@@ -0,0 +1,260 @@
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Watches',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Women\'s Watches',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Women\'s Wrist Watches',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Women\'s Smartwatches',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Men\'s Watches',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Men\'s Wrist Watches',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Men\'s Watch Bands',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Men\'s Pocket Watches',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Men\'s Smartwatches',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Girls\' Watches',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Girls\' Wrist Watches',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Boys\' Watches',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Boys\' Wrist Watches',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
