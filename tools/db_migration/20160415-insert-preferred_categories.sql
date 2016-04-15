111e33c0418d46262a3be8bc316da385f0ad5ddc
Jason Kim <jason.kim.jiho@gmail.com>
2016-04-15 15:59:46 -0400

    new pref. categories added

---
+++ tools/db_migration/20160415-insert-preferred_categories.sql
@@ -0,0 +1,220 @@
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Men\'s Watch Bands',
+	'70',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Women\'s Watch Bands',
+	'70',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Light Bulbs',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Compact Fluorescent Bulbs',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Fluorescent Tubes',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Halogen Bulbs',
+	'50',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'High Intensity Discharge Bulbs',
+	'50',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Incandescent Bulbs',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Krypton & Xenon Bulbs',
+	'50',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'LED Bulbs',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Black Light Bulbs',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
