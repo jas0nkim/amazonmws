8bdc45edb6d1713bc5f419182f3de8a78336a86d
Jason Kim <jason.kim.jiho@gmail.com>
2016-04-17 22:13:52 -0400

    added pref. categories

---
+++ tools/db_migration/20160417-insert-preferred_categories.sql
@@ -0,0 +1,160 @@
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Laptop Bags, Cases & Sleeves',
+	'50',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Laptop Backpacks',
+	'50',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Laptop Briefcases',
+	'50',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Laptop Folio Cases',
+	'50',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Laptop Messenger Bags',
+	'60',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Laptop Shoulder Bags',
+	'60',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Laptop Sleeves',
+	'70',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Tablet Keyboard Cases',
+	'50',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
