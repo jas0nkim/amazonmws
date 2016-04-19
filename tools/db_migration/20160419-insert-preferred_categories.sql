ac82e71f3dc83957c1cbadf28a82856fbdb3c1db
Jason Kim <jason.kim.jiho@gmail.com>
2016-04-19 11:41:19 -0400

    added pref. categories

---
+++ tools/db_migration/20160419-insert-preferred_categories.sql
@@ -0,0 +1,1080 @@
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Car Care',
+	'60',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Cleaning Kits',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Exterior Care Products',
+	'60',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Polishes & Waxes',
+	'60',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Chrome & Metal Polishes',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Chrome Polishes',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Metal Polishes',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Pre-Wax Cleaners',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Bug & Sap Removers',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Tar & Wax Removers',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Waxes',
+	'70',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Car Wash Equipment',
+	'70',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Car Washing Applicators',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Car Washing Buckets',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Car Washing Nozzles & Hose Attachments',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Car Washing Sponges & Mitts',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Waterless Car Washing Treatments',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Car Washing Windshield Squeegees',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Cleaners',
+	'70',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Polishing & Rubbing Compounds',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Polishing & Waxing Kits',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Exterior Sealants',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Cleaning Water Squeegee Blades',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Finishing Products',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Care Corrosion & Rust Inhibitors',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Protective Shields',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Glass Care Products',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Interior Care Products',
+	'70',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Carpet Cleaners',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Leather Care Products',
+	'70',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Protectants',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Plastic Care Products',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Rubber Care Products',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Vinyl Care Products',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Upholstery Care Products',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Vacuums',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Vinyl Cleaners',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Solvents',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Tire & Wheel Care Products',
+	'60',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Automotive Tire Care',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Automotive Wheel Care',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Tools & Equipment',
+	'80',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Air Dryers, Blowers & Blades',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Cleaning Brushes & Dusters',
+	'40',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Cleaning Cloths & Towels',
+	'80',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Cleaning Chamois',
+	'80',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Cleaning Drying Mitts',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Drying Pads',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Cleaning Microfiber',
+	'60',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Detailing Tools',
+	'30',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Machine Polishing Equipment',
+	'60',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Buffer & Polishing Backing Plates',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Body Repair Buffing & Polishing Pads',
+	'80',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Undercoatings',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
