9efbb45d5ca9520ace07c5e274a8c2bcd7c1fd61
Jason Kim <jason.kim.jiho@gmail.com>
2016-04-22 12:03:43 -0400

    renamed hotfix_a_to_e_category_map_null.py to hotfix_a_to_e_category_map_update_all.py - for updating all entries in a_to_e_category_map table

---
+++ atoe/bin/hotfix_a_to_e_category_map_update_all.py
@@ -0,0 +1,29 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'scrapers', 'amzn'))
+
+import re
+
+from amazonmws import django_cli
+django_cli.execute()
+
+from amazonmws import settings as amazonmws_settings, utils as amazonmws_utils
+from amazonmws.loggers import GrayLogger as logger
+from amazonmws.model_managers import *
+
+from atoe.helpers import CategoryHandler
+
+if __name__ == "__main__":
+    _rand_ebay_store = EbayStoreModelManager.fetch_one(random=True)
+    handler = CategoryHandler(ebay_store=_rand_ebay_store)
+    
+    atoe_map = AtoECategoryMapModelManager.fetch()
+    for cmap in atoe_map:
+        try:
+            ebay_category_id, ebay_category_name = handler.find_ebay_category(a.amazon_category)
+            AtoECategoryMapModelManager.update(cmap=cmap, 
+                ebay_category_id=ebay_category_id, 
+                ebay_category_name=ebay_category_name)
+        except Exception:
+            continue
+
