5920c0c9fbc20164192e6b681ab60dc5af9a967a
Jason Kim <jason.kim.jiho@gmail.com>
2016-04-23 12:33:15 -0400

    adding new pref. categories - interim

---
+++ tools/db_migration/20160423-insert-preferred_categories.sql
@@ -0,0 +1,40 @@
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Automotive Tools & Equipment',
+	'80',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Air Conditioning Tools & Equipment',
+	'80',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
