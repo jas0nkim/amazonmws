ed301162226ea93766d79844d2310a040b057c10
Jason Kim <jason.kim.jiho@gmail.com>
2016-05-05 15:34:51 -0400

    working on pref. categories

---
+++ tools/db_migration/20160505-insert-preferred_categories.sql
@@ -0,0 +1,500 @@
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Fasteners',
+	'70',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Anchors',
+	'50',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Anchor Bolts',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Concrete Screws',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Drive Anchors',
+	'2',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Drywall Anchors',
+	'10',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'Hollow-Wall Anchors',
+	'20',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
+INSERT INTO `ebay_store_preferred_categories` (
+	`ebay_store_id`,
+	`category_type`, 
+	`category_name`,
+	`max_items`,
+	`priority`,
+	`status`,
+	`created_at`,
+	`updated_at`
+) VALUES (
+	'1',
+	'amazon_bestseller',
+	'',
+	'',
+	'100',
+	'1',
+	NOW(),
+	NOW()
+);
+
