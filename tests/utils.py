c6fbb4f66761264263b61ecea68c915d59ec144f
Jason Kim <jason.kim.jiho@gmail.com>
2016-06-17 11:10:20 -0400

    test util.calculate_profitable_price script updated

--- tests/utils.py
+++ tests/utils.py
@@ -1,13 +1,23 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'rfi'))
 
 from decimal import Decimal
+
+###### setting django module ######
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'rfi.settings'
+
+import django
+django.setup()
+
+###################################
 
 from amazonmws import settings as amazonmws_settings, utils as amazonmws_utils
 from amazonmws.model_managers import *
 
-ebay_store_username = u'wbjworld'
-ebay_store = EbayStoreModelManager.fetch_one(username=ebay_store_username)
+ebay_store_id = 1
+ebay_store = EbayStoreModelManager.fetch_one(id=1)
 
-eb_price = amazonmws_utils.calculate_profitable_price(Decimal('26.99'), ebay_store)
+eb_price = amazonmws_utils.calculate_profitable_price(Decimal('10.80'), ebay_store)
 print eb_price
