911eafcff6577f01b2414bb261dfe8d72327dc9b
Jason Kim <jason.kim.jiho@gmail.com>
2016-09-09 22:42:18 -0400

    added task in cronjob

---
+++ tasks/revise-items-wo-crawl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PATH=/virtualenvs/amazonmws/bin:/home/jason/.local/bin:/home/jason/.local/bin:/home/jason/.local/bin:/home/jason/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+
+source /home/jason/.profile
+cd /applications/amazonmws/scrapers/amzn/amzn/tasks
+/virtualenvs/amazonmws/bin/python /applications/amazonmws/scrapers/amzn/amzn/tasks/revise_items_wo_crawl.py
