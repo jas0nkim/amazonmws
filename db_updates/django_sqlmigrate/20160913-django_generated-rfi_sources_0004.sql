2e77d788597b89b0dedc15cc3acc086f353b4b1c
Jason Kim <jason.kim.jiho@gmail.com>
2016-09-15 11:18:11 -0400

    amazon_items.variation_specifics column added

--- db_updates/django_sqlmigrate/20160913-django_generated-rfi_sources_0004.sql
+++ db_updates/django_sqlmigrate/20160913-django_generated-rfi_sources_0004.sql
@@ -4,6 +4,11 @@ BEGIN;
 --
 ALTER TABLE `amazon_items` ADD COLUMN `parent_asin` varchar(32) NULL;
 ALTER TABLE `amazon_items` ALTER COLUMN `parent_asin` DROP DEFAULT;
+--
+-- Add field variation_specifics to amazonitem
+--
+ALTER TABLE `amazon_items` ADD COLUMN `variation_specifics` varchar(255) NULL;
+ALTER TABLE `amazon_items` ALTER COLUMN `variation_specifics` DROP DEFAULT;
 CREATE INDEX `amazon_items_bb72ef9d` ON `amazon_items` (`parent_asin`);
 
 COMMIT;
