b18da01d25861f8359c0efbb07086c35df99f6a9
Jason Kim <jason.kim.jiho@gmail.com>
2016-09-16 12:20:50 -0400

    added db table - ebay_category_features

---
+++ db_updates/django_sqlmigrate/20160916-django_generated-rfi_listings_0009.sql
@@ -0,0 +1,17 @@
+BEGIN;
+--
+-- Create model EbayCategoryFeatures
+--
+CREATE TABLE `ebay_category_features` (
+    `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `ebay_category_id` varchar(100) NOT NULL UNIQUE,
+    `ebay_category_name` varchar(255) NULL,
+    `brand_mpn_identifier_enabled` bool NOT NULL,
+    `upc_enabled` varchar(100) NULL,
+    `variations_enabled` bool NOT NULL,
+    `created_at` datetime NOT NULL,
+    `updated_at` datetime NOT NULL,
+    `ts` datetime NOT NULL
+);
+
+COMMIT;
