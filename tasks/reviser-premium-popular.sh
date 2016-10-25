8e10d44c1b279fc0b503229e22409ea3011d6c24
Jason Kim <jason.kim.jiho@gmail.com>
2016-10-25 01:26:08 -0400

    crontab updated

--- tasks/reviser-premium-popular.sh
+++ tasks/reviser-premium-popular.sh
@@ -3,5 +3,5 @@
 PATH=/virtualenvs/amazonmws/bin:/home/jason/.local/bin:/home/jason/.local/bin:/home/jason/.local/bin:/home/jason/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
 source /home/jason/.profile
-cd /applications/amazonmws/scrapers/amzn/amzn/bin
-/virtualenvs/amazonmws/bin/python /applications/amazonmws/scrapers/amzn/amzn/bin/run_repricer.py -s premium -p slow
+cd /applications/amazonmws/scrapers/amzn/amzn/tasks
+/virtualenvs/amazonmws/bin/python /applications/amazonmws/scrapers/amzn/amzn/tasks/run_reviser.py -e 1 -p popular
--- tasks/reviser_sold-premium.sh
+++ tasks/reviser_sold-premium.sh
@@ -3,5 +3,5 @@
 PATH=/virtualenvs/amazonmws/bin:/home/jason/.local/bin:/home/jason/.local/bin:/home/jason/.local/bin:/home/jason/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
 source /home/jason/.profile
-cd /applications/amazonmws/scrapers/amzn/amzn/bin
-/virtualenvs/amazonmws/bin/python /applications/amazonmws/scrapers/amzn/amzn/bin/run_repricer_sold.py -s premium
+cd /applications/amazonmws/scrapers/amzn/amzn/tasks
+/virtualenvs/amazonmws/bin/python /applications/amazonmws/scrapers/amzn/amzn/tasks/run_reviser_sold.py -e 1
