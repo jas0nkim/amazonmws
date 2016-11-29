ab5049d6e9716ba6ceb02b8796d4fc22c10a0de3
Jason Kim <jason.kim.jiho@gmail.com>
2016-11-29 18:11:07 -0500

    bug fixed

--- atoe/tasks/ebay_orders_fetcher.py
+++ atoe/tasks/ebay_orders_fetcher.py
@@ -61,11 +61,6 @@ def __fetch_new_and_save_orders(ebay_store, since_hours_ago=4):
                 logger.error("[OrderID:{}] No sales record found from the order - {}".format(order.OrderID))
                 continue
 
-            payment_status = None
-            for payment in order.MonetaryDetails.Payments.Payment:
-                payment_status = payment.PaymentStatus
-                break
-
             # init/create ebay order
             ebay_order = EbayOrderModelManager.create(ebay_store=ebay_store,
                 order_id=order.OrderID,
@@ -85,7 +80,7 @@ def __fetch_new_and_save_orders(ebay_store, since_hours_ago=4):
                 buyer_shipping_phone=sale_record.ShippingAddress.get('Phone', '') if sale_record.ShippingAddress.has_key('Phone') else '', # optional
                 order_status=order.OrderStatus,
                 checkout_status=sale_record.OrderStatus.CheckoutStatus,
-                payment_status=payment_status,
+                payment_status=moded_order.MonetaryDetails.Payments.Payment.PaymentStatus,
                 creation_time=sale_record.CreationTime,
                 paid_time=sale_record.OrderStatus.get('PaidTime', None) if sale_record.OrderStatus.has_key('PaidTime') else None, # optional
                 feedback_left=False
@@ -109,17 +104,12 @@ def __update_order_status_if_exists(ebay_store, since_hours_ago=4):
             # check order already stored in db
             _existed_order = EbayOrderModelManager.fetch_one(order_id=moded_order.OrderID)
 
-            moded_order_payment_status = None
-            for _payment in moded_order.MonetaryDetails.Payments.Payment:
-                moded_order_payment_status = _payment.PaymentStatus
-                break
-
-            if _existed_order.order_status == moded_order.OrderStatus and _existed_order.payment_status == moded_order_payment_status:
+            if _existed_order.order_status == moded_order.OrderStatus and _existed_order.payment_status == moded_order.MonetaryDetails.Payments.Payment.PaymentStatus:
                 continue
 
             ebay_order = EbayOrderModelManager.update(order=_existed_order, 
                 order_status=moded_order.OrderStatus,
-                payment_status=moded_order_payment_status)
+                payment_status=moded_order.MonetaryDetails.Payments.Payment.PaymentStatus)
 
         except Exception as e:
             logger.exception("Failed to save ebay order - {}".format(str(e)))
