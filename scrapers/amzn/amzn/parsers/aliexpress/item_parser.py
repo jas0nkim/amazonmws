5fabcee8e7192175a2e58aa2bcd4a9c6f2e28ea7
Jason Kim <jason.kim.jiho@gmail.com>
2016-12-14 13:27:34 -0500

    bug fixed

--- scrapers/amzn/amzn/parsers/aliexpress/item_parser.py
+++ scrapers/amzn/amzn/parsers/aliexpress/item_parser.py
@@ -14,7 +14,7 @@ from amazonmws import settings as amazonmws_settings, utils as amazonmws_utils
 from amazonmws.loggers import GrayLogger as logger, StaticFieldFilter, get_logger_name
 from amazonmws.model_managers import *
 
-from amzn.items import AliexpressItem, AliexpressItemDescription
+from amzn.items import AliexpressItem, AliexpressItemDescription, AliexpressItemShipping
 
 
 class AliexpressItemParser(object):
