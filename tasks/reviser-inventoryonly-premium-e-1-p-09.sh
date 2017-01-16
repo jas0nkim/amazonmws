e06ccd075764d4802a490c803ea66bdb1742f6f2
Jason Kim <jason.kim.jiho@gmail.com>
2017-01-16 14:24:35 -0500

    seperated each tasks

---
+++ tasks/reviser-inventoryonly-premium-e-1-p-09.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PATH=/virtualenvs/amazonmws/bin:/home/jason/.local/bin:/home/jason/.local/bin:/home/jason/.local/bin:/home/jason/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+
+source /home/jason/.profile
+cd /applications/amazonmws/scrapers/amzn/amzn/tasks
+flock -n /applications/amazonmws/locks/reviser-e-1-p-09.lock -c '/virtualenvs/amazonmws/bin/python /applications/amazonmws/scrapers/amzn/amzn/tasks/run_reviser.py -i -e 1 -p 9 > /var/log/cron/reviser-e-1-p-09-`date +\%Y\%m\%d\%H\%M\%S`.log 2>&1'
