21603c84097a0875056439f27b42928160c6de1b
Jason Kim <jason.kim.jiho@gmail.com>
2017-02-21 11:42:38 -0500

    added crontab - ebay-order-returns-fetcher.sh

---
+++ tasks/ebay-order-returns-fetcher.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PATH=/virtualenvs/amazonmws/bin:/home/jason/.local/bin:/home/jason/.local/bin:/home/jason/.local/bin:/home/jason/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+
+source /home/jason/.profile
+cd /applications/amazonmws/atoe/tasks
+/virtualenvs/amazonmws/bin/python /applications/amazonmws/atoe/tasks/ebay_order_returns_fetcher.py
