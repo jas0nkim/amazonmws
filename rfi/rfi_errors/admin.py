79a44fb40823ea38d0d099200f731b3e468c676b
Jason Kim <jason.kim.jiho@gmail.com>
2018-01-23 14:31:28 -0500

    django admin - added trading api error screen

--- rfi/rfi_errors/admin.py
+++ rfi/rfi_errors/admin.py
@@ -1,3 +1,32 @@
+from __future__ import unicode_literals
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+
 from django.contrib import admin
 
-# Register your models here.
+from amazonmws import settings as amazonmws_settings
+
+from models import *
+
+
+class EbayTradingApiErrorAdmin(admin.ModelAdmin):
+    list_display = ('message_id', 'error_code', 'asin_link', 'ebid_link',)
+    list_filter = ('error_code',)
+    search_fields = ['message_id', 'error_code', 'asin', 'ebid',]
+
+    def asin_link(self, obj):
+        return "<a href=\"{url}\" target=\"_blank\">{asin}</a>".format(
+            url=amazonmws_settings.AMAZON_ITEM_VARIATION_LINK_FORMAT % obj.asin,
+            asin=obj.asin if obj.asin else "")
+
+    asin_link.allow_tags = True
+
+    def ebid_link(self, obj):
+        return "<a href=\"{url}\" target=\"_blank\">{ebid}</a>".format(
+            url=amazonmws_settings.EBAY_ITEM_LINK_FORMAT % obj.ebid,
+            ebid=obj.ebid if obj.ebid else "")
+
+    ebid_link.allow_tags = True
+
+admin.site.register(EbayTradingApiError, EbayTradingApiErrorAdmin)
